9cf40c34b0336bbd58e0eba3575bde46f8540aee
Andrea Hedström <andrea@Andreas-Air.lan>
2023-02-24 14:12:03 +0100

    added forecast and some styling

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
 const container = document.getElementById('weather-today');
 const bodyContainer = document.querySelector(".body-container");
 const message = document.getElementById("message")
+const forecast = document.getElementById("forecast")
 
 
-// fetch ('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=d023daa35568a6fdd2a5f549145a0497')
-//     .then((response) => {
-//         return response.json();
-//     })
-//     .then((json) => {
-//         console.log("it is working",json);
-//     })
-    
 
-//fetch ('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=d023daa35568a6fdd2a5f549145a0497')
-//.then((response) => {
-  // response.json();
-//})
-//.then((data) => {
-  // console.log("data", data);
-  //})
 
 fetch("https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=cc7a2bf72a818c2078266faf4fe15d7b")
  .then ((response) => {
@@ -26,12 +12,15 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=cc7a2bf
 })
  .then((json) => {
    console.log(json); 
-   container.innerHTML = `
 
+   const temperature = (json.main.temp.toFixed(0))
+
+   container.innerHTML = `
+   <p>Temp: ${temperature}°</p>
    <p>Sunrise: ${new Date(json.sys.sunrise * 1000)
-    .toLocaleTimeString()}</p>
+    .toLocaleTimeString([], {timeStyle: 'short'})}</p>
    <p>Sunset: ${new Date(json.sys.sunset * 1000)
-    .toLocaleTimeString()}</p>
+    .toLocaleTimeString([], {timeStyle: 'short'})}</p>
     `
 
 const weather = (json.weather[0].main) //If we declare weather here we can
@@ -52,20 +41,46 @@ else if (weather === "Clear") {
 }
 else {
     icon.src = "./Designs/Design-2/icons/noun_Cloud_1188486.svg"
-    message.innerHTML = `<h1>Something something ${json.name} is grey today.</h1>`
+    message.innerHTML = `<h1>Something and something ${json.name} is grey today.</h1>`
     bodyContainer.classList.add('clouds')
 }
 }
 todaysWeather()
 
-});
+
 // .catch((err) =>{ //ERROR function. We pass in a function as a parameter in the function, just like the then function.
 //     console.log(err)
 //   })
 
-//console.log("hello")
+
 
 
 
 
 //5 dagars prognos som ändras
+//After London in the link "&units=metric&" made it to celcius. 
+fetch ("https://api.openweathermap.org/data/2.5/forecast?q=London&units=metric&APPID=cc7a2bf72a818c2078266faf4fe15d7b")
+.then ((response) => {
+  return response.json ()
+})
+.then ((json) => {
+  console.log(json);
+
+  const filteredForecast = json.list.filter(item => item.dt_txt.includes('12:00:00'));
+  console.log(filteredForecast)
+
+  filteredForecast.forEach (json => {
+    const date = new Date (json.dt * 1000);
+    let dayName = date.toLocaleDateString("en-US", {weekday:"short"});
+
+  forecast.innerHTML += `
+  <p>${dayName}</p>
+  <p>${json.main.temp.toFixed(0)}°</p>
+  </div>
+  `;
+  console.log("Date", filteredForecast[1])
+  })
+
+  
+});
+}); 
